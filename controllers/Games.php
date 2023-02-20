1098c2649d4b1cb1b24026b45eebf4340fdf99a0
Kavindu <kavindupriyanath@gmail.com>
2023-02-20 17:11:51 +0530

    Kavindu New Branch

--- controllers/Games.php
+++ controllers/Games.php
@@ -26,28 +26,35 @@ class Games extends Controller
 
             if ($gameClassification == 'action') {
                 $this->view->games = $this->model->showClassifiedGames('action');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('action');
                 $this->view->title = "Category: Action";
             } else if ($gameClassification == 'adventure') {
                 $this->view->games = $this->model->showClassifiedGames('adventure');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('adventure');
                 $this->view->title = "Category: Adventure";
             } else if ($gameClassification == 'rpg') {
                 $this->view->games = $this->model->showClassifiedGames('rpg');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('rpg');
                 $this->view->title = "Category: RPG";
             } else if ($gameClassification == 'racing') {
                 $this->view->games = $this->model->showClassifiedGames('racing');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('racing');
                 $this->view->title = "Category: Racing";
             } else if ($gameClassification == 'simulation') {
                 $this->view->games = $this->model->showClassifiedGames('simulation');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('simulation');
                 $this->view->title = "Category: Simulation";
             } else if ($gameClassification == 'strategy') {
                 $this->view->games = $this->model->showClassifiedGames('strategy');
+                $this->view->gamesPagesCount = $this->model->totalGamesPageCount('strategy');
                 $this->view->title = "Category: Strategy";
             }
         } else {
             $this->view->games = $this->model->showAllGames($start, $maxLimit);
+            $this->view->gamesPagesCount = $this->model->totalGamesPageCount(null);
         }
 
-        $this->view->gamesPagesCount = $this->model->totalGamesPageCount();
+
 
         $this->view->render('Main/Games');
     }
