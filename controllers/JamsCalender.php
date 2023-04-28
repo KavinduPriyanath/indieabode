8ecfeb84f4cb81178e4a978e7a661df8c91d5549
Kavindu <kavindupriyanath@gmail.com>
2023-04-29 01:21:30 +0530

    Jam Calender Row 1 done

--- controllers/JamsCalender.php
+++ controllers/JamsCalender.php
@@ -23,6 +23,28 @@ class JamsCalender extends Controller
 
         $this->view->months = $visibleMonthList;
 
+        $allJams = $this->model->GetGamejams($currentMonth);
+        $jamsCount = count($allJams);
+
+        $row1 = [];
+        $row2 = [];
+
+        for ($i = 0; $i < $jamsCount; $i++) {
+
+            if ($i == 0) {
+                array_push($row1, $allJams[$i]);
+            } else if ($allJams[$i]['submissionStartDate'] < $allJams[$i - 1]['votingEndDate']) {
+                array_push($row2, $allJams[$i]);
+            } else if ($allJams[$i]['submissionStartDate'] > $allJams[$i - 1]['votingEndDate']) {
+                array_push($row1, $allJams[$i]);
+            }
+        }
+
+        $this->view->firstRow = $row1;
+        $this->view->secondRow = $row2;
+
+        // print_r($row1);
+
         $this->view->render('GameJam/JamCalender');
     }
 }
