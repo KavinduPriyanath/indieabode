2cd4b59065b9039112be4165496c128b8d70e4f6
nadee-darshika <88460737+nadee-darshika@users.noreply.github.com>
2023-05-10 12:14:26 +0530

    gig db changes

--- models/Admin_GigD_Model.php
+++ models/Admin_GigD_Model.php
@@ -7,48 +7,29 @@ class Admin_GigD_Model extends Model
     {
         parent::__construct();
     }
+    
+    function getNoofGigs(){
+        $sql = "SELECT COUNT(*) AS orderedGigs, (SELECT COUNT(*) FROM gig WHERE gigStatus=0) AS notorderedGigs 
+        FROM gig WHERE gigStatus=0";
 
+        $stmt = $this->db->prepare($sql);
 
-    // public function getData($tabale_name,$days)
-    // {
+        $stmt->execute();
 
-    //     $select = "SELECT 0 n ";
-    //     for ($i = 1; $i < $days; $i++) {
-    //       $select=$select." UNION SELECT ".$i;
-    //     }
-    //     $sql = "
-    //     SELECT 
-    //       sub.date, 
-    //       COALESCE(tbl.count, 0) AS count
-    //     FROM 
-    //       (SELECT 
-    //         DATE(NOW() - INTERVAL (".$days." - n.n) DAY) AS date
-    //        FROM 
-    //         (".$select.") n
-    //       ) sub
-    //     LEFT JOIN
-    //       (SELECT 
-    //         DATE(created_at) AS date, 
-    //         COUNT(gamerID) AS count
-    //        FROM 
-    //         ".$tabale_name."
-    //        WHERE 
-    //         created_at > CURRENT_DATE()-".$days." AND created_at < CURRENT_DATE()-1
-    //        GROUP BY 
-    //         DATE(created_at)
-    //       ) tbl
-    //     ON 
-    //       sub.date = tbl.date
-    //     ORDER BY 
-    //       sub.date;
-    //     ";
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
-     
+        $total_ended_gigs = $row['orderedGigs'];
+        $total_ongoing_gigs = $row['notorderedGigs'];
 
-    //     // WHERE 
-    //     //     created_at >= NOW() - INTERVAL ".$days." DAY
-    //     //    GROUP BY 
-    //     //     DATE(created_at)
+        return array('orderedGigs' => $total_ended_gigs, 'notorderedGigs' => $total_ongoing_gigs);
+    }
 
-
+    function getTotalTx() {
+        $sql = "SELECT ROUND(SUM(publisherCost), 2) AS totalTx FROM gig_purchases";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute();
+        $row = $stmt->fetch(PDO::FETCH_ASSOC);
+        $totalTx = $row['totalTx'];
+        return $totalTx;
+    }
 }
